fc7727d7e6c4e548009106936abf009e1d391751
Harrison Tyson <Harrison@Computer.local>
2020-06-27 12:46:50 +1200

    Index Full CSS for Cards

--- inc/info-cards.php
+++ inc/info-cards.php
@@ -1,31 +1,42 @@
 <div class="info-cards">
   <div class="card">
-    <h3>Our Mission</h3>
+    <div class="card-head" id="our-mission">
+      <h3>Our Mission</h3>
+    </div>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
       ea commodo consequat.
     </p>
+    <a href="#">Read More...</a>
   </div>
 
   <div class="card">
-    <h3>Most Trusted</h3>
+    <div class="card-head" id="most-trusted">
+      <h3>Most Trusted</h3>
+    </div>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
       ea commodo consequat.
     </p>
+    <a href="#">Read More...</a>
   </div>
 
   <div class="card">
-    <h3>Enquire Today</h3>
+    <div class="card-head" id="enquire-today">
+      <h3>Enquire Today</h3>
+    </div>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
       ea commodo consequat.
     </p>
+    <a href="#">Read More...</a>
+
+
   </div>
 </div>
