7b1df9d1ec937ad2fb3017c8dcaa81cc18dbe452
Harrison Tyson <Harrison@Computer.local>
2020-07-03 11:27:44 +1200

    Index CSS Footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,38 +1,54 @@
 <div class="footer">
-  <a href="#body"><img src="" alt="Return to Top"></a> <!-- Up arrow ^ of some kind -->
+  <a href="#body"><img src="./media/return-to-top.png" alt="Return to Top"></a> <!-- Up arrow ^ of some kind -->
 
   <div class="footer-content">
     <div class="column">
       <!-- Newsletter Subscription -->
       <div class="newsletter">
         <h4>Subscribe to Our Newsletter</h4>
-        <form method="post">
+        <form class="subscribe-form" method="post">
           <input type="text" name="subscribe-email" placeholder="Email Address">
-          <input type="submit" name="subscribe-button" value="Sign Up">
+          <input type="submit" name="subscribe-button" value="Subscribe">
         </form>
       </div>
 
     <!-- Social Media Links -->
     <div class="media-links">
-      Facebook Twitter YouTube Instagram
+      <a href="https://www.facebook.com/PharcourtsTimaru/" target="_blank"><img src="./media/facebook-icon.png" alt="Facebook"></a>
+      <a href="https://twitter.com/pharcourtsnz?lang=en" target="_blank"><img src="./media/twitter-icon.png" alt="Twitter"></a>
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank"><img src="./media/youtube-icon.png" alt="YouTube"></a>
+      <a href="https://www.instagram.com/Pharcourts/" target="_blank"><img src="./media/instagram-icon.png" alt="Instagram"></a>
     </div>
 
     </div>
 
   <!-- Customer Quotes/Testimonials -->
-    <div class="column">
-      Fancy slideshow customer testomonials
-    </div>
+    <div class="column testimonials">
+      <div class="slider-testimonial">
+        <div class="testimonial">
+          <p>"Lorem. Ipsum. Dolor."</p>
+          <i>- A Happy Customer</i>
+        </div>
 
+        <div class="testimonial">
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</p>
+          <i>- A Sastisfied Customer</i>
+        </div>
+
+        <div class="testimonial">
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+             Suspendisse a vulputate ex, quis tristique lectus."</p>
+          <i>- A Professional Reviewer</i>
+        </div>
+
+    </div>
+</div>
     <!-- Quality of Life/Legal -->
-    <div class="column">
-      <ul>
-        <li>Privacy Policy</li>
-        <li>Terms and Conditions</li>
-        <li>Feedback</li>
-      </ul>
+    <div class="column legal-information">
+      <h5><a href="#">Privacy Policy</a></h5>
+      <h5><a href="#">Terms and Conditions</a></h5>
+      <h5><a href="#">Feedback</a></h5>
 
     </div>
   </div>
-  © H. Tyson 2020
 </div>
