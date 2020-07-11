c9353824f2e846186a886730547766cb90cdc47c
Harrison Tyson <harrisontyson@Harrisons-MacBook-Air.local>
2020-07-11 15:47:18 +1200

    Added About Us Page

---
+++ about-us.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <!-- Head content -->
+  <?php include "./inc/head.php"; ?>
+
+  <body id="body">
+
+    <!-- Navigation -->
+    <?php include "./inc/nav.php"; ?>
+
+    <!-- Title Section -->
+    <?php include "./inc/title.php"; ?>
+
+    <div class="content column">
+      <?php include "./inc/about-mission.php"; ?>
+      <hr class="solid">
+      <?php include "./inc/about-story.php"; ?>
+    </div>
+
+    <!-- Footer -->
+     <?php include "./inc/footer.php"; ?>
+  </body>
+</html>
---
+++ inc/about-mission.php
@@ -0,0 +1,7 @@
+<div class="about-info column">
+  <h1>Our Mission</h1>
+  <div class="about-mission row">
+    <p>Our Mission is to always do right by our customers to provide you the best service and the best homes.</p>
+    <img src="./media/about-mission.png" alt="Our Mission">
+  </div>
+</div>
