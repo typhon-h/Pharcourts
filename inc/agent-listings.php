10ab1cce0247c45391a38ff02fa6ed7973115216
Harrison Tyson <harrisontyson@Harrisons-MacBook-Air.local>
2020-07-21 21:37:23 +1200

    Added toggle for Sold listings to Agent Profile

--- inc/agent-listings.php
+++ inc/agent-listings.php
@@ -1,13 +1,29 @@
 
+<?php
+if (isset($_POST['switch-listings'])){
+  $current_state = secure($_POST['switch-state']);
+  if($current_state == "Active"){ //Switch values around
+    $switch_state = "Sold";
+  }
+  else{
+    $switch_state = "Active";
+  }
+}
 
+if(!isset($current_state) || !isset($switch_state)){ //Set Default values
+  $current_state = "Active";
+  $switch_state = "Sold";
+}
+ ?>
 
-<h1>My Active Listings</h1>
+<div class="listings-heading column">
+  <h1>My <?php echo $current_state ?> Listings</h1>
+  <form method="post">
+    <input type="text" name="switch-state" value="<?php echo $switch_state; ?>" hidden>
 
-
-
-
-
-
+    <input type="submit" class="form-field" name="switch-listings" value="View <?php echo $switch_state ?> Listings">
+  </form>
+</div>
 
 <div class="listings-items row">
   <?php
@@ -15,7 +31,7 @@
                      FROM tbl_listings
                      INNER JOIN tbl_properties
                      ON tbl_listings.Property = tbl_properties.PID
-                     WHERE tbl_listings.Sold = false
+                     WHERE tbl_listings.Sold = ".(($current_state == "Active")? 0:1)."
                         AND tbl_listings.Agent = {$agent['AID']}";
   //Run Query
   $result = $conn -> query($listings_query);
@@ -25,7 +41,7 @@
   }
 
   if(empty($listings)){
-    echo 'No Active Listings';
+    echo "No {$current_state} Listings";
   }
   //Display Listings
   foreach($listings as $listing){
