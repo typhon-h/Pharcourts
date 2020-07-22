cf3177ccec28b527203a192d7678213003c1a278
Harrison Tyson <harrisontyson@Harrisons-MacBook-Air.local>
2020-07-22 20:24:29 +1200

    Added Responsive Design (Mobile)

--- inc/head.php
+++ inc/head.php
@@ -3,6 +3,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="./css/style-mobile.css">
   <link rel="shortcut icon" type="image/x-icon" href="./media/favicon.ico" />
   <title>Pharcourts</title>
   <?php
